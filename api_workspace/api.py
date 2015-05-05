91b65488c1559e7e3f5c289c2edeb7e9796d72db
NickyThreeNames <nich.cm@gmail.com>
2015-05-05 16:11:43 +0000

    Continue project and add functions

--- api_workspace/api.py
+++ api_workspace/api.py
@@ -25,7 +25,42 @@ class CPIData(object):
         internally.
 
         """
+        fp = requests.get(url, stream=True, headers={'Accept-Encoding': None}).raw
+        if save_as_file is None:
+            return self.load_from_file(fp)
+        else:
+            with open(save_as_file 'wb+') as out:
+                while True:
+                    buffer = fp.read(81920)
+                    if not buffer:
+                        break
+                    out.write(buffer)
+                with open(save_as_file) as fp:
+                    return self.load_from_file(fp)
+
     def load_from_file(self, fp):
+        current_year = None
+        year_cpi = []
+        for line in fp:
+            while not line.startswith("Date "):
+                pass
+            data = line.rstrip().split()
+            year = int(data[0].split("-")[0])
+            cpi = float(data[1])
+
+            if self.first_year is None:
+                self.first_year = year
+            self.last_year = year
+
+            if current_year != year:
+                if current_year is not None:
+                    self.year_cpi[current_year] = sum(year_cpi)/len(year_cpi)
+                year_cpi = []
+                current_year = year
+            year_cpi.append(cpi)
+        
+        if current_year is not None and current_year not in self.year_cpi:
+            self.year_cpi[current_year] = sum(year_cpi)/len(year_cpi)
 
 
 def main():
